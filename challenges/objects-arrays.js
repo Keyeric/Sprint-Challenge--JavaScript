ff7e712746beb3c6535ecf3e9a0964ddefab741f
Key Bristol <keyeric.bristol@gmail.com>
2019-12-20 11:13:54 -0500

    Array 3 Request Passed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -98,6 +98,12 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+
+graduates.forEach(function(univ){
+  if (univ.university.includes('Uni')){
+    unisWithUni.push(`${univ.university}`);
+  }
+});
 console.log(unisWithUni);
 
 
