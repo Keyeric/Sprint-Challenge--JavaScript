d5cc952745d93f3a0b298c5ab8d748fbc0a62ac3
Key Bristol <keyeric.bristol@gmail.com>
2019-12-20 11:11:28 -0500

    Array 1 Request Passed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,6 +74,11 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+
+graduates.forEach(function(school){
+  universities.push(`${school.university}`);
+  universities.sort();
+});
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
