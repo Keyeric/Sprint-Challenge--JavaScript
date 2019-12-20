81d1ada3b408637321a6ef40c7833bd452eb5f79
Key Bristol <keyeric.bristol@gmail.com>
2019-12-20 11:40:21 -0500

    Callbacks Step 1 Passed

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,8 +6,10 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
-
+function consume(a, b, cb) 
+{
+  return callback(a, b);
+}
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
