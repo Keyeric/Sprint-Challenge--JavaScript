9bb8dfe2fd1e0cda10d114b807cf3e947c8c2703
Key Bristol <keyeric.bristol@gmail.com>
2019-12-20 11:45:36 -0500

    Callbacks Step 3 Passed

--- challenges/functions.js
+++ challenges/functions.js
@@ -8,7 +8,7 @@
 */
 function consume(a, b, cb) 
 {
-  return callback(a, b);
+  return cb(a, b);
 }
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -26,9 +26,9 @@ function greeting(first, last){
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
