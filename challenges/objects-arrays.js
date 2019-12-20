77ab1251b7e5ca7ce79badc41833d8642d82283d
Key Bristol <keyeric.bristol@gmail.com>
2019-12-20 11:08:08 -0500

    flappy chungus flew

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -26,7 +26,14 @@ const steg =
   period: 'Late Jurassic'
 };
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const velo =
+  {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+};
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
